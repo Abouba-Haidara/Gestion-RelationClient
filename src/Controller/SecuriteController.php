399fb4c54fe2ee3abe17179e04c2d008a6941b01
Yemini Coder <31940004+Abouba-Haidara@users.noreply.github.com>
2020-05-24 20:55:21 +0000

    Update SecuriteController.php

--- src/Controller/SecuriteController.php
+++ src/Controller/SecuriteController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Events;
 use App\Entity\User;
 use Twig\Environment;
+use App\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\EventDispatcher\GenericEvent;
@@ -15,14 +17,32 @@ class SecuriteController  extends AbstractController
 {
 
     /**
-     * @Route("/TestEvent", name="sec_login")
+     * @Route("/login", name="sec_login")
      */
-    public function login(EventDispatcherInterface $eventDispatcher): Response
+    public function login(): Response
     {
-
-        //On déclenche l'event
-        $event = new GenericEvent(new User());
-        $eventDispatcher->dispatch(Events::USER_REGISTERED, $event);
         return $this->render('/pages/sec/login.html.twig');
+    }
+    /**
+     * @Route("/registration", name="sec_registration")
+     */
+    public function registration(Request $request, EventDispatcherInterface $eventDispatcher): Response
+    {
+        $user =  new User();
+        $form =  $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($user);
+            $this->em->flush();
+            $this->addFlash('success', 'Le client a été bien ajouté!');
+            //On déclenche l'event
+            $event = new GenericEvent($user);
+            $eventDispatcher->dispatch(Events::USER_REGISTERED, $event);
+            $this->addFlash('success', 'un message vous a  été envoyé.');
+            return $this->redirectToRoute('sec_registration');
+        }
+
+        return $this->render('/pages/sec/register.html.twig');
     }
 }
